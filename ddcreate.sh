1a6b8bf172c1f044368b198a31b5d1a04ed9fbe1
dderichswei <dderichswei@ddansible2.container.demo.local>
2020-05-25 13:19:22 +0200

    nginx added

--- ddcreate.sh
+++ ddcreate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker build -t "ddsynergy:1" .
-docker run -d --name "ddsynergy" --restart unless-stopped -v $(pwd):/home/notebook/notebooks -v /persistent/osdepl:/persistent/osdepl -p 8842:8888 ddsynergy:1
+docker run -d --name "ddsynergy" --restart unless-stopped -v $(pwd):/home/notebook/notebooks -v /persistent/osdepl:/persistent/osdepl -p 8888:8888 ddsynergy:1
+mkdir $pwd/html
+docker run -d --name "nginx" --restart unless-stopped -v $(pwd)/html:/usr/share/nginx/html/media -p 80:80 nginx
 docker logs -f ddsynergy
